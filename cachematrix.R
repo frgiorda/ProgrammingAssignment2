03ccae186ca0b0b1fc5a400112c9206d97855d2b
francesca <francescagiordano80@gmail.com>
2015-02-18 22:18:15 +0100

    usingdet

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
-## prova script 
-## Write a short comment describing this function
+##  Calculate matrix inversion and save it 
 
+##  ---- Create special matrix for cache memory 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    ## create matrix with same dimensions as input matrix
+    inv <- list()
+    length(inv) <- length(x)
+    dim(inv) <-  dim(x)
+    
+    det <-   NULL 
+    set <- function(y) {
+        x <<- y
+        inv <- list()
+        length(inv) <- length(x)
+        dim(inv) <-  dim(x)
+        det <-   NULL 
+    }
+    
+    ## function that sets input matrix (x) determinant 
+    setdet <- function(determ) det <<- determ
+    ## function that gets input matrix determinant
+    getdet <- function() det
+    
+    ## function that gets the input matrix
+    get <- function() x
+    ## function that sets the inverse of input matrix
+    setinv <- function(inverse) inv <<- inverse
+    ## funtion that gets the inverse of input matrix
+    getinv <- function() inv
+    ## define special matrix
+    list(set = set, get = get, 
+         setdet = setdet, getdet = getdet,
+         setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## ---
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## get inverse and determinant of special matrix
+    inv <- x$getinv()
+    det <- x$getdet()
+    
+    ## if determinant exists, get inverse from cached data
+    if(!is.null(det)) {
+       message("getting cached data")    
+       # return cached inverse matrix
+       return(inv)
+    }
+    ## if determinant is NULL, get input matrix
+    data <- x$get()
+    ## invert input matrix
+    inv <- solve(data, ...)
+    ## calculate input matrix determinant
+    det <- det(data)
+    ##save into cached data the inverse matrix just calculated
+    x$setinv(inv)
+    ## and the determinant just calculated
+    x$setdet(det)
+    ##return inverse matrix just calculated
+    inv
 }
